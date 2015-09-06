9565f46e8cd4f5e0ecfe813774fc02cbab09a7cb
DavidG2897 <david.g.2897@gmail.com>
2015-09-06 12:31:22 -0500

    Create wsq03.py

---
+++ wsq03.py
@@ -0,0 +1,17 @@
+x = input("Input first integer: ")
+y = input("Input second integer: ")
+x = int(x)
+y = int(y)
+
+a = x + y
+b = x - y
+c = x * y
+d = x // y
+e = x % y
+print(
+    "The sum is:",a,
+    "The difference is:",b)
+print("The multiplication is:",c,
+    "The integer division is:",d,
+    "The remainder is:",e
+    )
