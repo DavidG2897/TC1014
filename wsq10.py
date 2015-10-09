1f0b70241babe64b0db900e0ac7d3f9553fd0250
DavidG2897 <david.g.2897@gmail.com>
2015-10-08 21:53:53 -0500

    Create wsq10.py

---
+++ wsq10.py
@@ -0,0 +1,9 @@
+from statistics import *    
+
+list = [float(input("1st number: ")), float(input("2nd number: ")), float(input("3rd number: ")), float(input("4th number: ")),
+        float(input("5th number: ")), float(input("6th number: ")), float(input("7th number: ")), float(input("8th number: ")),
+        float(input("9th number: ")), float(input("10th number: "))]
+
+print ("The total is: ", sum(list))
+print ("The mean of the list is: ",mean (list))
+print ("The average deviations es: ", pstdev (list))
